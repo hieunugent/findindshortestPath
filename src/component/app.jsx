58de65c1ca0e4b6c331080ece850f8dbb8ac0efc
Hieu Nguyen <hieunugent@gmail.com>
2020-05-25 22:29:02 -0700

    create raw grid with 20 rows and 50 cols

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -2,32 +2,46 @@ import React, {useState} from "react";
 import Node from "./Node/Node";
 
 
-
+const getInitialGrid = () =>{
+    const grid =[];
+    for(let row = 0; row < 20; row++){
+        const currentRow=[];
+        for(let col = 0; col< 50; col++){
+            currentRow.push(createNode(col, row));
+        }
+        grid.push(currentRow);
+    }
+    return grid;
+};
+const createNode= (col, row) => {
+    return {
+        col, 
+        row,
+    };
+};
 
 
 function App(){
-    const getInitialGrid = () =>{
-        const grid =[];
-        for(let row = 0; row < 20; row++){
-            const currentRow=[];
-            for(let col = 0; col< 50; col++){
-                currentRow.push(createNode(col, row));
-            }
-            grid.push(currentRow);
-        }
-        return grid;
-    };
-    const createNode= (col, row) => {
-        return {
-            col, 
-            row,
-        };
-    };
-   return (<div>
-              <Node/>
-             </div>
-            
-        
+    const grid = getInitialGrid();
+   return (<div className="grid">
+             {grid.map((row, rowIdx)=> {
+                 return (
+                    <div  key={rowIdx}>
+                        {row.map((node, nodeIdx)=> {
+                            const {row, col} = node;
+                            return (
+                                <Node
+                                    key={nodeIdx}
+                                    col={col}
+                                    row={row}
+                                />
+                            );
+                        })}
+                    </div>
+                 );
+             })}
+             
+             </div>        
     )
 }
 export default App;
